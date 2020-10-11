d578734e8601e2540f8a51d4d34112318bab3924
Malik Edwards <malikedwards777@gmail.com>
2020-10-11 14:36:18 -0500

    Exercise 4 Completed

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -5,6 +5,7 @@ window.onload = function() {
     const squares = board.children;
     let game_state = Array(squares.length).fill(null);
     
+    
     for(let count=0; count<squares.length;count++){
         let square = squares[count];
         let attr = document.createAttribute("location");
@@ -29,8 +30,11 @@ window.onload = function() {
                 square.textContent='X';
             }//Endif
 
+            
             //set postion in game state to value X or O
             game_state[square.getAttribute('location')]=square.textContent;
+            
+            checkWinner(Number(square.getAttribute('location')),square.textContent);
             // console.log(game_state);
             // console.log(element.getAttribute('location'));
             // console.log(Object.values(squares).indexOf(element));
@@ -47,6 +51,23 @@ window.onload = function() {
     }//End of For
 
 
+    function checkWinner(location,play){
+        let status = document.getElementById('status');
+        let p = [[0,1,2],[0,3,6],[0,4,8],
+                [1,4,7],[2,5,8],[2,4,6],
+                [3,4,5],[6,7,8]]
+    
+        //Determines Winner
+        for(let x=0;x<p.length;x++){
+            if(p[x].includes(location)){
+                if((game_state[p[x][0]]===play) && (game_state[p[x][1]]===play) &&(game_state[p[x][2]]===play)){
+                    status.classList.add('you-won');
+                    status.textContent=`Congratulations! ${play} is the Winner!`
+                    return;
+                }
+            }//ednd if
+        }//end for
+    }//end checkWinner 
 }//End of onLoad
     
     // board.querySelectorAll(".square").forEach((element)=>{
