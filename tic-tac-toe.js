4a26d89b61207379730dcd47a11bff8e2e18b8cb
Malik Edwards <malikedwards777@gmail.com>
2020-10-11 11:38:36 -0500

    Exercise 2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,10 +3,43 @@
 window.onload = function() {
     const board = document.getElementById("board");
     const squares = board.children;
-    // console.log(squares[0])
-    // squares[0].className="square";
-    // console.log(squares[0])
+    let game_state = Array(squares.length).fill(null);
+    
     for(let count=0; count<squares.length;count++){
-        squares[count].className="square";
+        let square = squares[count];
+        let attr = document.createAttribute("location");
+        attr.value = count;
+
+        square.className="square";
+        square.setAttributeNode(attr);
+
+        square.addEventListener("click", function() {
+            let classes=square.classList;
+
+            if(classes.contains('X')){
+                classes.replace('X','O');
+                square.textContent='O';
+            }
+            else if(classes.contains('O')){
+                classes.replace('O','X');
+                square.textContent='X';
+            }
+            else{
+                classes.add('X');
+                square.textContent='X';
+            }
+
+            //set postion in game state to value X or O
+            game_state[square.getAttribute('location')]=square.textContent;
+            // console.log(game_state);
+            // console.log(element.getAttribute('location'));
+            // console.log(Object.values(squares).indexOf(element));
+        });
     }
+
+
 }
+
+    
+    // board.querySelectorAll(".square").forEach((element)=>{
+    // });
