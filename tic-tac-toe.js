0cfa50e35ce2468d73391aaab782b1d7766aa8aa
Malik Edwards <malikedwards777@gmail.com>
2020-10-11 18:27:59 -0500

    Exercise 6 Completed

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -5,52 +5,10 @@ window.onload = function() {
     const squares = board.children;
     let game_state = Array(squares.length).fill(null);
     const message = document.getElementById('status').textContent;
-    
-    
-    for(let count=0; count<squares.length;count++){
-        let square = squares[count];
-        let attr = document.createAttribute("location");
-        attr.value = count;
-
-        square.className="square";
-        square.setAttributeNode(attr);
-
-        square.addEventListener("click", function() {
-            let classes=square.classList;
-
-            if(classes.contains('X')){
-                classes.replace('X','O');
-                square.textContent='O';
-            }
-            else if(classes.contains('O')){
-                classes.replace('O','X');
-                square.textContent='X';
-            }
-            else{
-                classes.add('X');
-                square.textContent='X';
-            }//Endif
-
-            
-            //set postion in game state to value X or O
-            game_state[square.getAttribute('location')]=square.textContent;
-            
-        });//end Click
-
-
-        square.addEventListener('mouseenter',function(){
-             square.classList.add('hover');   
-        });//End mouse enter
-
-        square.addEventListener('mouseleave',function(){
-            square.classList.remove('hover')
-            if (square.textContent!==""){
-                checkWinner(Number(square.getAttribute('location')),square.textContent);
-            }
-        });//end mouse leave
-    }//End of For
-
     const button = document.querySelector("button");
+
+    startGame();
+
     button.addEventListener('click',function(){
         game_state = Array(squares.length).fill(null);
         for (const square of squares) {
@@ -62,8 +20,55 @@ window.onload = function() {
         let status = document.getElementById('status');
         status.textContent=message;
         status.classList.remove('you-won');
+        startGame();
     });
 
+
+    function startGame(){
+        for(let count=0; count<squares.length;count++){
+            let square = squares[count];
+            let attr = document.createAttribute("location");
+            attr.value = count;
+    
+            square.className="square";
+            square.setAttributeNode(attr);
+    
+            square.onclick=function() {
+                let classes=square.classList;
+    
+                if(classes.contains('X')){
+                    classes.replace('X','O');
+                    square.textContent='O';
+                }
+                else if(classes.contains('O')){
+                    classes.replace('O','X');
+                    square.textContent='X';
+                }
+                else{
+                    classes.add('X');
+                    square.textContent='X';
+                }//Endif
+    
+                
+                //set postion in game state to value X or O
+                game_state[square.getAttribute('location')]=square.textContent;
+                
+            };//end Click
+    
+    
+            square.onmouseenter = function(){
+                 square.classList.add('hover');   
+            };//End mouse enter
+    
+            square.onmouseleave = function(){
+                square.classList.remove('hover')
+                if (square.textContent!==""){
+                    checkWinner(Number(square.getAttribute('location')),square.textContent);
+                    square.onclick=null;
+                }
+            };//end mouse leave
+        }//End of For
+    }//End start game
 
     function checkWinner(location,play){
         let status = document.getElementById('status');
@@ -77,6 +82,12 @@ window.onload = function() {
                 if((game_state[p[x][0]]===play) && (game_state[p[x][1]]===play) &&(game_state[p[x][2]]===play)){
                     status.classList.add('you-won');
                     status.textContent=`Congratulations! ${play} is the Winner!`
+
+                    for (const square of squares) {
+                        square.onclick=null;
+                        square.onmouseenter=null;
+                        square.onmouseleave=null;
+                    }
                     return;
                 }
             }//ednd if
