957d0bd52da8c383f7c83cefb9d73b2d0a4a1fab
Malik Edwards <malikedwards777@gmail.com>
2021-01-01 16:48:30 -0500

    Added feature to make X default player. And let player alternate when square is clicked instead of being decided by players

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -6,6 +6,7 @@ window.onload = function() {
     let game_state = Array(squares.length).fill(null);
     const message = document.getElementById('status').textContent;
     const button = document.querySelector("button");
+    let player;
 
     startGame();
 
@@ -25,6 +26,7 @@ window.onload = function() {
 
 
     function startGame(){
+        player = 'X';   //set default player to X
         for(let count=0; count<squares.length;count++){
             let square = squares[count];
             let attr = document.createAttribute("location");
@@ -32,28 +34,23 @@ window.onload = function() {
     
             square.className="square";
             square.setAttributeNode(attr);
-    
+            
             square.onclick=function() {
                 let classes=square.classList;
-    
-                if(classes.contains('X')){
-                    classes.replace('X','O');
-                    square.textContent='O';
-                }
-                else if(classes.contains('O')){
-                    classes.replace('O','X');
-                    square.textContent='X';
-                }
-                else{
-                    classes.add('X');
-                    square.textContent='X';
-                }//Endif
-    
-                
+                square.textContent=player;
+                classes.add(player);
                 //set postion in game state to value X or O
-                game_state[square.getAttribute('location')]=square.textContent;
-                
-            };//end Click
+                game_state[square.getAttribute('location')]=player;
+
+                checkWinner(squares,game_state,Number(square.getAttribute('location')),player);
+                square.onclick=null;
+                    
+                //changes control to next player
+                if (player === 'X')
+                    player = 'O';
+                else
+                    player = 'X';
+            };//end square.onclick
     
     
             square.onmouseenter = function(){
@@ -61,39 +58,35 @@ window.onload = function() {
             };//End mouse enter
     
             square.onmouseleave = function(){
-                square.classList.remove('hover')
-                if (square.textContent!==""){
-                    checkWinner(Number(square.getAttribute('location')),square.textContent);
-                    square.onclick=null;
-                }
+                square.classList.remove('hover');
             };//end mouse leave
         }//End of For
     }//End start game
 
-    function checkWinner(location,play){
-        let status = document.getElementById('status');
-        let p = [[0,1,2],[0,3,6],[0,4,8],
-                [1,4,7],[2,5,8],[2,4,6],
-                [3,4,5],[6,7,8]]
-    
-        //Determines Winner
-        for(let x=0;x<p.length;x++){
-            if(p[x].includes(location)){
-                if((game_state[p[x][0]]===play) && (game_state[p[x][1]]===play) &&(game_state[p[x][2]]===play)){
-                    status.classList.add('you-won');
-                    status.textContent=`Congratulations! ${play} is the Winner!`
 
-                    for (const square of squares) {
-                        square.onclick=null;
-                        square.onmouseenter=null;
-                        square.onmouseleave=null;
-                    }
-                    return;
-                }
-            }//ednd if
-        }//end for
-    }//end checkWinner 
 }//End of onLoad
-    
-    // board.querySelectorAll(".square").forEach((element)=>{
-    // });
+
+//checlks for game winner
+function checkWinner(squares,game_state,location,play){
+    let status = document.getElementById('status');
+    let p = [[0,1,2],[0,3,6],[0,4,8],
+            [1,4,7],[2,5,8],[2,4,6],
+            [3,4,5],[6,7,8]]
+
+    //Determines Winner
+    for(let x=0;x<p.length;x++){
+        if(p[x].includes(location)){
+            if((game_state[p[x][0]]===play) && (game_state[p[x][1]]===play) &&(game_state[p[x][2]]===play)){
+                status.classList.add('you-won');
+                status.textContent=`Congratulations! ${play} is the Winner!`
+
+                for (const square of squares) {
+                    square.onclick=null;
+                    square.onmouseenter=null;
+                    square.onmouseleave=null;
+                }
+                return;
+            }
+        }//ednd if
+    }//end for
+}//end checkWinner
